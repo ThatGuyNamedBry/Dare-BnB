a7f8f16b0bf97bc34d5111888d9ee51b70b03cc5
BryantS <bryantstine@yahoo.com>
2023-05-16 15:18:43 -0700

    fixed typos in migration files

--- backend/db/migrations/20230516000004-create-booking.js
+++ backend/db/migrations/20230516000004-create-booking.js
@@ -18,7 +18,7 @@ module.exports = {
       spotId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'Spot',
+          model: 'Spots',
           key: 'id'
         },
         onUpdate: 'CASCADE',
@@ -27,7 +27,7 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'User',
+          model: 'Users',
           key: 'id'
         },
         onUpdate: 'CASCADE',
--- backend/db/migrations/20230516000005-create-review-image.js
+++ backend/db/migrations/20230516000005-create-review-image.js
@@ -18,7 +18,7 @@ module.exports = {
       reviewId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'Review',
+          model: 'Reviews',
           key: 'id'
         },
         onUpdate: 'CASCADE',
