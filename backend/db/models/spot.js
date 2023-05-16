8d5588b675a77a1aecb92356173e689c5f0caadb
BryantS <bryantstine@yahoo.com>
2023-05-16 14:06:53 -0700

    working on db issues

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,11 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Spot.belongsTo(models.User, { foreignKey: 'ownerId' });
+      Spot.belongsToMany(models.User, { through: models.Booking, foreignKey: 'spotId' });
       Spot.hasMany(models.Review, { foreignKey: 'spotId' });
-      Spot.hasMany(models.Booking, { foreignKey: 'spotId' });
       Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' });
-
-
     }
   }
   Spot.init({
--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -12,7 +12,6 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       SpotImage.belongsTo(models.Spot, { foreignKey: 'spotId' });
-
     }
   }
   SpotImage.init({
