17ab34444130d08f1a0f0148746acf143b9718c9
BryantS <bryantstine@yahoo.com>
2023-05-17 13:54:13 -0700

    fixed spot seeder and utils auth

--- backend/db/seeders/20230516222602-demo-spots.js
+++ backend/db/seeders/20230516222602-demo-spots.js
@@ -18,6 +18,45 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    options.tableName = 'Spots';
+    return queryInterface.bulkInsert(options, [
+      {
+        ownerId: 1,
+        address: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA',
+        lat: 40.7128,
+        lng: -74.0060,
+        name: 'Cozy Apartment',
+        description: 'A comfortable apartment in the heart of the city.',
+        price: 100
+      },
+      {
+        ownerId: 2,
+        address: '456 Main St',
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA',
+        lat: 37.7749,
+        lng: -122.4194,
+        name: 'Beach House',
+        description: 'A beautiful house near the beach.',
+        price: 200
+      },
+      {
+        ownerId: 3,
+        address: '789 Main St',
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA',
+        lat: 39.77,
+        lng: -110.41,
+        name: 'Golden Gate House',
+        description: 'A brand new house near the Golden Gate Park.',
+        price: 200
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
@@ -27,5 +66,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'Spots';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      ownerId: { [Op.in]: ['1', '2', '3'] }
+    }, {});
   }
 };
