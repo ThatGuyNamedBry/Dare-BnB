956fe70d32dcfdf7503c9da6a37585d8d99f9e13
BryantS <bryantstine@yahoo.com>
2023-05-19 13:16:38 -0700

    working on deployment issues get all spots

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -10,53 +10,125 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const review = require('../../db/models/review');
 
+
+// // Get all of the Current User's Bookings
+// router.get('/current', requireAuth, async (req, res, next) => {
+//   const { user } = req;
+
+//   try {
+//     const bookings = await Booking.findAll({
+//       where: {
+//         userId: user.id
+//       },
+//       include: [
+//         {
+//           model: Spot,
+//           attributes: [
+//             'id',
+//             'ownerId',
+//             'address',
+//             'city',
+//             'state',
+//             'country',
+//             'lat',
+//             'lng',
+//             'name',
+//             'price',
+//             [Sequelize.literal('(SELECT "url" FROM "SpotImages" WHERE "SpotImages"."spotId" = "Spot"."id" AND "SpotImages"."preview" = true LIMIT 1)'), 'previewImage']
+//           ]
+//         }
+//       ],
+//       attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt']
+//     });
+
+//     //Reorder the response
+//     const formattedBookings = bookings.map(booking => ({
+//       id: booking.id,
+//       spotId: booking.spotId,
+//       Spot: booking.Spot,
+//       userId: booking.userId,
+//       startDate: booking.startDate,
+//       endDate: booking.endDate,
+//       createdAt: booking.createdAt,
+//       updatedAt: booking.updatedAt
+//     }));
+
+//     res.status(200).json({ Bookings: formattedBookings });
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
 // Get all of the Current User's Bookings
 router.get('/current', requireAuth, async (req, res, next) => {
-    const { user } = req;
+  const { user } = req;
 
-    try {
-      const bookings = await Booking.findAll({
-        where: {
-          userId: user.id
-        },
-        include: [
-          {
-            model: Spot,
-            attributes: [
-              'id',
-              'ownerId',
-              'address',
-              'city',
-              'state',
-              'country',
-              'lat',
-              'lng',
-              'name',
-              'price',
-              [Sequelize.literal('(SELECT "url" FROM "SpotImages" WHERE "SpotImages"."spotId" = "Spot"."id" AND "SpotImages"."preview" = true LIMIT 1)'), 'previewImage']
-            ]
-          }
-        ],
-        attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt']
-      });
+  try {
+    const bookings = await Booking.findAll({
+      where: {
+        userId: user.id
+      },
+      include: [
+        {
+          model: Spot,
+          attributes: [
+            'id',
+            'ownerId',
+            'address',
+            'city',
+            'state',
+            'country',
+            'lat',
+            'lng',
+            'name',
+            'price'
+          ],
+          include: [
+            {
+              model: SpotImage,
+              as: 'SpotImages',
+              attributes: ['url'],
+              where: {
+                preview: true
+              },
+              required: false
+            }
+          ]
+        }
+      ],
+      attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt']
+    });
 
-      //Reorder the response
-      const formattedBookings = bookings.map(booking => ({
-        id: booking.id,
-        spotId: booking.spotId,
-        Spot: booking.Spot,
-        userId: booking.userId,
-        startDate: booking.startDate,
-        endDate: booking.endDate,
-        createdAt: booking.createdAt,
-        updatedAt: booking.updatedAt
-      }));
+    // Reorder the response and add previewImage field
+    const formattedBookings = bookings.map(booking => ({
+      id: booking.id,
+      spotId: booking.spotId,
+      Spot: {
+        id: booking.Spot.id,
+        ownerId: booking.Spot.ownerId,
+        address: booking.Spot.address,
+        city: booking.Spot.city,
+        state: booking.Spot.state,
+        country: booking.Spot.country,
+        lat: booking.Spot.lat,
+        lng: booking.Spot.lng,
+        name: booking.Spot.name,
+        price: booking.Spot.price,
+        previewImage: booking.Spot.SpotImages[0]?.url || 'img url'
+      },
+      userId: booking.userId,
+      startDate: booking.startDate,
+      endDate: booking.endDate,
+      createdAt: booking.createdAt,
+      updatedAt: booking.updatedAt
+    }));
 
-      res.status(200).json({ Bookings: formattedBookings });
-    } catch (error) {
-      return next(error);
-    }
+    res.status(200).json({ Bookings: formattedBookings });
+  } catch (error) {
+    return next(error);
+  }
 });
+
+
 
 // Edit a booking
 router.put('/:bookingId', requireAuth, async (req, res, next) => {
