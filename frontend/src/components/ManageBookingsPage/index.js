3e2df8ceb1017d4c2e607133429766be1ae034cc
BryantS <bryantstine@yahoo.com>
2023-08-23 13:54:45 -0700

    managebookingpage styling

--- frontend/src/components/ManageBookingsPage/index.js
+++ frontend/src/components/ManageBookingsPage/index.js
@@ -12,6 +12,21 @@ const ManageBookingsPage = () => {
     dispatch(getCurrentUserAllBookingsThunk());
   }, [dispatch]);
 
+  const lengthOfStay = (startDate, endDate) => {
+    return Math.floor((new Date(endDate) - new Date(startDate)) / (24 * 60 * 60 * 1000)) + 1;
+  };
+
+  const formatDateRange = (startDate, endDate) => {
+    const formattedStartDate = new Date(startDate).toLocaleDateString(undefined, { month: 'short', day: 'numeric' });
+    const formattedEndDate = new Date(endDate).toLocaleDateString(undefined, { month: 'short', day: 'numeric' });
+
+    if (formattedStartDate === formattedEndDate) {
+      return formattedStartDate;
+    } else {
+      return `${formattedStartDate} - ${formattedEndDate}`;
+    }
+  };
+
   return (
     <div>
       <h1>Manage Bookings</h1>
@@ -22,15 +37,19 @@ const ManageBookingsPage = () => {
               <img src={booking.Spot.previewImage} alt={'Spot Thumbnail'} />
             </Link>
             <div>{booking.Spot.name}</div>
-            <div>Start Date: {booking.startDate}</div>
-            <div>End Date: {booking.endDate}</div>
-            <div>Booking Price: {/* Calculate booking price based on your logic */}</div>
+            <div>Date: {formatDateRange(booking.startDate, booking.endDate)}</div>
+            <div>Length of stay: {lengthOfStay(booking.startDate, booking.endDate)} {lengthOfStay(booking.startDate, booking.endDate) === 1 ? 'day' : 'days'}</div>
+            <div>Booking Cost: ${lengthOfStay(booking.startDate, booking.endDate) * booking.Spot.price}</div>
             <div id='UpdateDeleteButtons'>
               {/* Update button */}
               <Link to={`/bookings/${booking.id}/edit`}>
                 <button>Update</button>
               </Link>
               {/* Delete button */}
+              {/* <OpenModalButton
+                modalComponent={<DeleteConfirmationModal booking={booking} />}
+                buttonText="Delete"
+              /> */}
             </div>
           </li>
         ))}
